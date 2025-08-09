662a2f57188672417a6f0ffe3a425ac4867fd002
bryanzak77 <149523028+bryanzak77@users.noreply.github.com>
2025-08-09 14:03:29 +0000

    Interfaces

---
+++ interface.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MovableObjectsApp
+{
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Interfaces");
+
+            Car myCar = new Car();
+            Console.Write("My car: ");
+            myCar.Move();
+
+            Bicycle myBicycle = new Bicycle();
+            Console.Write("My bicycle: ");
+            myBicycle.Move();
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
